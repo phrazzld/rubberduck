e4fd4220cd0b161952580dbcecf653cd582d08dc
Phaedrus <phraznikov@gmail.com>
2019-10-19 19:32:53 -0700

    Add goodnight cognitive behavioral therapy feature, move to JSON-based config

--- config.go
+++ config.go
@@ -4,93 +4,167 @@ package main
 
 import (
 	"bufio"
+    "io/ioutil"
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
+    "strconv"
 )
 
-func getEditorConfig(reader *bufio.Reader) string {
+type Config struct {
+	Editor          string `json:"editor"`
+    TerminalHistoryEnabled bool `json:"terminalHistoryEnabled"`
+    TerminalHistoryFile string `json:"terminalHistoryFile"`
+    TerminalHistoryNumLines int `json:"terminalHistoryNumLines"`
+	GoodnightPrompts []string `json:"goodnightPrompts"`
+}
+
+func (conf *Config) SetEditor(editor string, configPath string) error {
+    conf.Editor = editor
+    err := writeConfigObjectToFile(*conf, configPath)
+    return err
+}
+
+func (conf *Config) SetTerminalHistoryEnabled(enabled bool, configPath string) error {
+    conf.TerminalHistoryEnabled = enabled
+    err := writeConfigObjectToFile(*conf, configPath)
+    return err
+}
+
+func (conf *Config) SetTerminalHistoryFile(historyFile string, configPath string) error {
+    conf.TerminalHistoryFile = historyFile
+    err := writeConfigObjectToFile(*conf, configPath)
+    return err
+}
+
+func (conf *Config) SetTerminalHistoryNumLines(numLines int, configPath string) error {
+    conf.TerminalHistoryNumLines = numLines
+    err := writeConfigObjectToFile(*conf, configPath)
+    return err
+}
+
+func (conf *Config) SetGoodnightPrompts(questions []string, configPath string) error {
+    conf.GoodnightPrompts = questions
+    err := writeConfigObjectToFile(*conf, configPath)
+    return err
+}
+
+func writeConfigObjectToFile(conf Config, configPath string) error {
+    jsonConf, err := json.MarshalIndent(conf, "", "  ")
+    if err != nil {
+        return err
+    }
+    err = ioutil.WriteFile(configPath, jsonConf, 0644)
+    return err
+}
+
+func setDefaultConfiguration(configPath string) error {
+    conf := Config{
+        Editor: "vim",
+        TerminalHistoryEnabled: false,
+        TerminalHistoryFile: "",
+        TerminalHistoryNumLines: 0,
+        GoodnightPrompts: []string{
+            "What did you achieve today?",
+            "What could you have done better?",
+            "What are you going to do tomorrow?",
+        },
+    }
+    err := writeConfigObjectToFile(conf, configPath)
+    return err
+}
+
+func loadConfiguration(configPath string) (Config, error) {
+	var conf Config
+    if !Exists(configPath) {
+        err := setDefaultConfiguration(configPath)
+        if err != nil {
+            return conf, err
+        }
+    }
+	configFile, err := os.Open(configPath)
+	defer configFile.Close()
+	if err != nil {
+		return conf, err
+	}
+	jsonParser := json.NewDecoder(configFile)
+	err = jsonParser.Decode(&conf)
+	return conf, err
+}
+
+func configEditor(configPath string, reader *bufio.Reader, conf Config) (Config, error) {
 	fmt.Print("Editor command (vim, nano, open, ...): ")
 	editor, err := reader.ReadString('\n')
 	if err != nil {
-		fmt.Println(err)
+        return conf, err
 	}
-	editor = strings.Replace(editor, "\n", "", -1)
-	editorConfig := "EDITOR=" + editor
-	// Configure Goyo (secretly)
-	if editor == "vim+" {
-		editorConfig = "EDITOR=vim\nGOYO=true"
-	}
-	return editorConfig
+    editor = stripNewlines(editor)
+    conf.SetEditor(editor, configPath)
+    err = writeConfigObjectToFile(conf, configPath)
+    return conf, err
 }
 
-func getHistoryLinesConfig(reader *bufio.Reader) string {
-	historyLinesConfig := "HISTORY_LINES="
-	// Get amount of history to include by default
-	fmt.Print("Number of lines of history to include: ")
-	numLines, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
-	}
-	historyLinesConfig += strings.Replace(numLines, "\n", "", -1)
-	return historyLinesConfig
+func stripNewlines(s string) string {
+    return strings.ToLower(strings.Replace(s, "\n", "", -1))
 }
 
-func getHistoryFileConfig(reader *bufio.Reader) string {
-	historyFileConfig := "HISTORY_FILE="
-	// Get history file name
-	fmt.Print("Name of terminal history file (.bash_history, .zsh_history, ...): ")
-	historyFile, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
-	}
-	historyFileConfig += strings.Replace(historyFile, "\n", "", -1)
-	return historyFileConfig
-}
-
-func disableHistoryConfig() string {
-	historyConfig := "HISTORY_FILE=NONE"
-	historyConfig += "\nHISTORY_LINES=0"
-	return historyConfig
-}
-
-func enableHistoryConfig(reader *bufio.Reader) string {
-	historyConfig := getHistoryFileConfig(reader) + "\n"
-	historyConfig += getHistoryLinesConfig(reader) + "\n"
-	return historyConfig
-}
-
-func getShellHistoryConfig(reader *bufio.Reader) string {
-	historyConfig := ""
-	fmt.Print("Include terminal history in journal entries? (y/n): ")
-	includeHistory, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
-	}
-	includeHistory = strings.ToLower(strings.Replace(includeHistory, "\n", "", -1))
-	switch includeHistory {
-	case "n":
-		historyConfig += disableHistoryConfig()
-	case "y":
-		historyConfig += enableHistoryConfig(reader)
-	default:
+func configTerminalHistory(configPath string, reader *bufio.Reader, conf Config) (Config, error) {
+    fmt.Print("Enable terminal history in journal entries? (y/n): ")
+    enableHistory, err := reader.ReadString('\n')
+    if err != nil {
+        return conf, err
+    }
+    enableHistory = stripNewlines(enableHistory)
+    switch enableHistory {
+    case "n":
+        conf.SetTerminalHistoryEnabled(false, configPath)
+        conf.SetTerminalHistoryFile("", configPath)
+        conf.SetTerminalHistoryNumLines(0, configPath)
+    case "y":
+        conf.SetTerminalHistoryEnabled(true, configPath)
+        fmt.Print("What is the name of your history file? (.bash_history, .zsh_history, ...): ")
+        historyFile, err := reader.ReadString('\n')
+        if err != nil {
+            return conf, err
+        }
+        historyFile = stripNewlines(historyFile)
+        conf.SetTerminalHistoryFile(historyFile, configPath)
+        fmt.Print("Lines of history to include: ")
+        numLinesStr, err := reader.ReadString('\n')
+        if err != nil {
+            return conf, err
+        }
+        numLinesStr = stripNewlines(numLinesStr)
+        numLines, err := strconv.Atoi(numLinesStr)
+        if err != nil {
+            return conf, err
+        }
+        conf.SetTerminalHistoryNumLines(numLines, configPath)
+    default:
 		fmt.Println("y/n: It means \"Enter the character 'y' or the character 'n'\"")
-	}
-	return historyConfig
+    }
+    err = writeConfigObjectToFile(conf, configPath)
+    return conf, err
 }
 
-func config(configPath string) {
+func config(configPath string) error {
 	reader := bufio.NewReader(os.Stdin)
+    conf, err := loadConfiguration(configPath)
+    if err != nil {
+        return err
+    }
 	// Configure editor
-	editorConfig := getEditorConfig(reader)
+	conf, err = configEditor(configPath, reader, conf)
+    if err != nil {
+        return err
+    }
 	// Toggle terminal history in stamps
-	historyConfig := getShellHistoryConfig(reader)
-	config := editorConfig + "\n" + historyConfig + "\n"
-	f, err := os.Create(configPath)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer f.Close()
-	f.WriteString(config)
-	f.Sync()
+	conf, err = configTerminalHistory(configPath, reader, conf)
+    if err != nil {
+        return err
+    }
+    // Save config to file
+    err = writeConfigObjectToFile(conf, configPath)
+    return err
 }
--- main.go
+++ main.go
@@ -16,13 +16,19 @@ func main() {
 		fmt.Println(err)
 	}
 	notesPath := filepath.Join(usr.HomeDir, "rubberducks")
-	confPath := filepath.Join(notesPath, "config")
+	confPath := filepath.Join(notesPath, "config.json")
 	if len(os.Args) == 1 {
-		rubberduck(confPath)
+		err = rubberduck(confPath)
+        if err != nil {
+            fmt.Println(err)
+        }
 	} else {
 		switch os.Args[1] {
 		case "config":
-			config(confPath)
+			err = config(confPath)
+            if err != nil {
+                fmt.Println(err)
+            }
 		case "review":
 			review(confPath)
 		case "reminisce":
@@ -30,6 +36,11 @@ func main() {
 		case "search":
 			hits := search(notesPath, os.Args[2:])
 			formatSearchResults(hits)
+		case "goodnight":
+			err = goodnight(confPath)
+            if err != nil {
+                fmt.Println(err)
+            }
 		default:
 			fmt.Println("Unrecognized command")
 			os.Exit(1)
